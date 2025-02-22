6d450e9d4c51c6d8238b3dfcc61ca1d82f4ffff2
mohamed <mezoattalla123@gmail.com>
2025-02-22 15:42:27 +0200

    ✨ [FEAT](components): Add AppColors and AppTextStyles classes.

---
+++ lib/components/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color greenDark = Color(0xFF417C1E);
+  static const Color greenWhite = Color(0xFF70B01C);
+  static const Color orange = Color(0xFFFC9E27);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color greyDarker = Color(0xFF464855);
+  static const Color textPrimary = Color(0xFF303030);
+  static const Color textSecondary = Color(0xFF2D3447);
+  static const Color border = Color(0xffC1C1C1);
+  static const Color accentColor = Color(0xFF6480F7);
+}
